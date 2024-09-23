92bf17f9f43de92839bbf16233e3900b039b4fa4
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2024-09-23 02:14:14 -0300

    feat(service): update ClientService class - remove synchronization between the client entity and the database during the transaction

--- src/main/java/com/studies/domain/service/ClientService.java
+++ src/main/java/com/studies/domain/service/ClientService.java
@@ -15,9 +15,17 @@ public class ClientService {
         return entityManager.find(Client.class, id);
     }
 
+    /**
+     * O persist recebe a nova entidade e a torna gerenciada para que seja inserida na base de dados
+     * O merge recebe a entidade e a torna uma cópia dessa entidade gerenciada. Qualquer alteração nessa cópia dentro do begin e commit, será registrada no banco de dados
+     * para impedir esta sincronização é precisa utilizar o método detach
+     */
+
     public Client save(Client  client){
         entityManager.getTransaction().begin();
         client = entityManager.merge(client);
+        entityManager.detach(client);
+        client.setName("Alterando entidade gerenciada");
         entityManager.getTransaction().commit();
         return client;
     }
