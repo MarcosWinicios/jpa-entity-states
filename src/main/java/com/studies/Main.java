9d5e89c3f056f2fb985323d7c4827c980df33be6
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2024-09-23 01:04:08 -0300

    test(Client): update Main class - test findById method of the Client Service class

--- src/main/java/com/studies/Main.java
+++ src/main/java/com/studies/Main.java
@@ -1,7 +1,18 @@
 package com.studies;
 
+import com.studies.domain.model.Client;
+import com.studies.domain.service.ClientService;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("Clients-PU");
+        ClientService clientService =  new ClientService(entityManagerFactory);
+        Client c1 = clientService.findById(1);
+        System.out.println(c1);
+
+        entityManagerFactory.close();
     }
 }
