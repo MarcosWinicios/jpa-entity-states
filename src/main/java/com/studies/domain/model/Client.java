5745d11c7ffaa2cbd3273c21b81a3a52022e3cca
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2024-09-23 00:58:02 -0300

    feat(model): create model - create Client class

---
+++ src/main/java/com/studies/domain/model/Client.java
@@ -0,0 +1,38 @@
+package com.studies.domain.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_client")
+public class Client {
+
+    @Id
+    private Integer id;
+    private String name;
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
