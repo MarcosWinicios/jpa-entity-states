3a49fd1887240719670951dd91102086a8a94125
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2024-09-23 02:01:25 -0300

    test(Client): update Main class - refactor test class

--- src/main/java/com/studies/Main.java
+++ src/main/java/com/studies/Main.java
@@ -2,7 +2,6 @@ package com.studies;
 
 import com.studies.domain.model.Client;
 import com.studies.domain.service.ClientService;
-import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
@@ -10,13 +9,24 @@ public class Main {
     public static void main(String[] args) {
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("Clients-PU");
         ClientService clientService =  new ClientService(entityManagerFactory);
+        //Consultar um cliente
         Client c1 = clientService.findById(1);
         System.out.println(c1);
 
-        Client c2 = new Client("Armazen Feliz");
-        clientService.create(c2);
+        //Criar novo cliente
+        Client c2 = clientService.save(new Client("Armazen Feliz"));
 
-        clientService.remove(c1);
+        //Atualizar cliente
+        c2.setName(c2.getName() + " atualizado");
+        clientService.save(c2);
+
+        //Remover um cliente
+        Client c3 = clientService.save(new Client("Cliente a ser excluído"));
+        System.out.println(c3.toString());
+        clientService.remove(c3);
+
+
+
         clientService.close();
         entityManagerFactory.close();
     }
